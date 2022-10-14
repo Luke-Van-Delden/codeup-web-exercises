16979d22c272b0fc803a93f9c1f5713651ffa766
Luke Van Delden <vandelden.luke@gmail.com>
2022-10-14 11:45:00 -0500

    for loop #4 completed

--- js/for_loops.js
+++ js/for_loops.js
@@ -13,3 +13,12 @@ for (i = 1; i < 11; i++){
     }
     console.log(((Math.floor(Math.random() * 181) + 20)) + " is " + evenodd);
 }
+
+for (i = 1; i < 10; i++){
+    let istring = i.toString()
+    let final = "";
+    for (x= 0; x < i ; x++) {
+        final = final + istring;
+    }
+    console.log(final)
+}
