fd29edd7d662ff91243b5cb6b0a19753db891f8a
Luke Van Delden <vandelden.luke@gmail.com>
2022-10-18 15:11:17 -0500

    bonus completed

--- js/planets-string.js
+++ js/planets-string.js
@@ -16,7 +16,7 @@
     console.log(linebr)
 
     /**
-     * TODO:
+     * TODOne:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      *
@@ -27,6 +27,40 @@
      * string, and <li> tags around each planet.
      */
 
+    let bonusString = planetsArray;
+    let newString = bonusString.join(`<li>`);
+    newString = "<ul><li>" + newString + "</li></ul>";
+    console.log(newString);
+// THIS IS A SOLUTION?
+    // planetsArray.unshift(("<ul>"));
+    // planetsArray.push(("</ul>"));
+    // planetsArray.forEach(function(planet) {
+    //     if (planet !== "<ul>" && planet !== "</ul>")
+    //     planet = "<li>" + planet + "</li>";
+    //     console.log(planet);
+    // })
+
+
+
+//    var linebr = planetsArray.join("<br>")
+//    var one = planetsArray.join("</li><li> ");
+//     console.log(one)
+//
+//     var two = one.split(" ");
+//     console.log(two)
+//     two.unshift("<ul><li>");
+//     console.log(two)
+//     two.push("</li></ul> ")
+//     console.log(two);
+//
+//   let three = two.join(" ")
+//     console.log(three)
+
+    //  var lico = lic.push("</li></ul>");
+//     var lic =
+
+    // USE push and unshift for final <ul></ul>
+    //console.log(planetsArray)
 
 
 })();
