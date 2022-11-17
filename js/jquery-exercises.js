95e365a9bc44be5445494e968a13df127e75ef5f
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-17 16:10:47 -0500

    H1 turn firebrick on click

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,8 +1,3 @@
-$(function() {
-    let header = $("h1").html();
-    $("li").css("font-size", "20px")
-    $("h1, li, p, div").css("color", "firebrick")
-    $("p").css("text-decoration", "underline")
-    $("div").css("font-weight", "bolder")
-    alert(header);
+$("h1").click(function() {
+    $("h1").css("background-color", "firebrick");
 });
