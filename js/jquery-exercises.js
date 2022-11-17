ac684318649f5d4515df5596432797ddc7b48188
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-17 16:50:01 -0500

    completted jquery mouse events

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,3 +1,15 @@
 $("h1").click(function() {
     $("h1").css("background-color", "firebrick");
 });
+
+$("p").dblclick(function() {
+    $("p").css("font-size", "18px");
+});
+
+$("li").hover(
+    function() {
+    $("li").css("color", "red");
+},
+    function() {
+        $("li").css("color", "black");
+    });
