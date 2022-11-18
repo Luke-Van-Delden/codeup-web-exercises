de58f70063813bc415b19a602fbbcef78ee46528
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-18 16:39:24 -0500

    Completed functionality for Konami Code

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,33 @@
 "use strict";
-$(document).keyup(function(event){
-    let series = event.key;
-    console.log(event.key);
-    if (series === "ArrowUp") {
-        $("#success").html("You have gained 30 lives and innnnnnnnfinite continues.");
-    }
-});
 
-
+var userInput = [];
 var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"];
+
+$(document).keyup(function(event){
+    var keyStroke = event.key;
+    userInput.push(keyStroke);
+    console.log(event.key);
+    if (userInput.length === konamiCode.length){
+        if (userInput.toString() === konamiCode.toString()) {
+            $("#success").html("You have gained 30 lives and innnnnnnnfinite continues.");
+        }
+        else {
+            $("#success").html("Close but no cigar");
+            console.log(userInput);
+            console.log(konamiCode);
+            userInput = [];
+        }
+    }
+    if (keyStroke === "Enter" && userInput.length !== konamiCode.length){
+        $("#success").html("Invalid code Entered");
+        userInput = [];
+    }
+    // COULD USE A COUNTER = 0, ++ ON KEYSTROKE ENTER
+    //
+    // if (series === "ArrowUp") {
+    //     $("#success").html("You have gained 30 lives and innnnnnnnfinite continues.");
+    // }
+});
 
 
 // ↑ ↑ ↓ ↓ ← → ← → B A
