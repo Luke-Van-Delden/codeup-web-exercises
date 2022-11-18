db93243645ac512f73d8c43660ac24fc89d34c86
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-18 15:59:32 -0500

    Created Konami files, started array

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+"use strict";
+$(document).keyup(function(event){
+    console.log(event.key);
+});
+var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"];
+
+
+// ↑ ↑ ↓ ↓ ← → ← → B A
