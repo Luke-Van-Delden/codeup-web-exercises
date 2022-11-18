a9d2b4fee4f329aee3ae49c36b9567e96f014c3c
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-18 16:10:18 -0500

    function checks event key

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,13 @@
 "use strict";
 $(document).keyup(function(event){
+    let series = event.key;
     console.log(event.key);
+    if (series === "ArrowUp") {
+        $("#success").html("You have gained 30 lives and innnnnnnnfinite continues.");
+    }
 });
+
+
 var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"];
 
 
