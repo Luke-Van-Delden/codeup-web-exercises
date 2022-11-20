4ecaf60300bad606e1ac68985204556bc9b896b3
Luke Van Delden <vandelden.luke@gmail.com>
2022-11-20 15:31:34 -0500

    updated code using indexOf

--- js/jquery.js
+++ js/jquery.js
@@ -1,35 +1,50 @@
 "use strict";
 
 var userInput = [];
+// var dinosaurInput = [];
 var konamiCode = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"];
 var blitzBigHead = ["2", "0", "0", "ArrowRight", "Enter"];
 var dinosaursForHire = ["c", "a", "ArrowLeft", "ArrowRight", "a", "b", "Enter"]
-
+// TODO: Write using indexOf, using unshift, to check for certain lengths by creating more userinput arrays.
+// TODO: Change Div on contra code
 $(document).keyup(function (event) {
     var keyStroke = event.key;
     userInput.push(keyStroke);
-    console.log(event.key);
-    if (userInput.length === konamiCode.length) {
-        if (userInput.toString() === konamiCode.toString()) {
+    // console.log(event.key);
+    // dinosaurInput.push(keyStroke);
+    console.log(keyStroke);
+    if (userInput.toString().indexOf(konamiCode.toString()) > -1) {
             $("#success").html("You have gained 30 lives and innnnnnnnfinite continues.");
-            userInput = [];
-        }
+            $("#image").html("<a href=''><img src='images/contra.jpeg' alt='contra'></a>");
+
     }
-    if (userInput.length === blitzBigHead.length) {
-        if (userInput.toString() === blitzBigHead.toString()) {
+    // if (userInput.length === blitzBigHead.length) {
+    //     if (userInput.toString() === blitzBigHead.toString()) {
+    //         $("#success").html("With the LATE HIT!").css("color", "firebrick");
+    //         $("#image").html("<a href='https://youtu.be/3IPZZvaKcV0?t=6'><img src='images/blitz.jpeg' alt='BigHead'></a>");
+    //         userInput = [];
+    //     }
+    // }
+
+    // if (userInput.length === blitzBigHead.length) {
+        if (userInput.toString().indexOf(blitzBigHead.toString()) > -1) {
             $("#success").html("With the LATE HIT!").css("color", "firebrick");
             $("#image").html("<a href='https://youtu.be/3IPZZvaKcV0?t=6'><img src='images/blitz.jpeg' alt='BigHead'></a>");
-            userInput = [];
         }
-    }
+    // }
+
+
+
     if (userInput.length === dinosaursForHire.length) {
         if (userInput.toString() === dinosaursForHire.toString()) {
             $("#success").html("Dinosaurs with guns... What's cooler than that???").css("color", "firebrick");
             $("#image").html("<a href='https://youtu.be/_AxxAJYcD64?t=160'><img src='images/Dinosaurs_For_Hire.webp' alt='BigHead'></a>");
-            userInput = [];
+            // dinosaurInput = [];
+            // userInput = [];
+            // console.log(dinosaurInput);
         }
     }
-    if (keyStroke === "Enter") {
-        userInput = [];
-    }
+    // if (keyStroke === "Enter") {
+    //     userInput = [];
+    // }
 });
